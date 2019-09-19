0ce825543a6b9df2826ae0ebceeb160f2386dd07
Nik <bor.tverd@gmail.com>
2019-09-19 21:31:17 +0300

    get projec

---
+++ Notes/Notes/Data/NoteDataBase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Notes.Models;
+
+namespace Notes.Data
+{
+	public class NoteDatabase
+	{
+		readonly SQLiteAsyncConnection _database;
+
+		public NoteDatabase(string dbPath)
+		{
+			_database = new SQLiteAsyncConnection(dbPath);
+			_database.CreateTableAsync<Note>().Wait();
+		}
+
+		public Task<List<Note>> GetNotesAsync()
+		{
+			return _database.Table<Note>().ToListAsync();
+		}
+
+		public Task<Note> GetNoteAsync(int id)
+		{
+			return _database.Table<Note>()
+				.Where(i => i.ID == id)
+				.FirstOrDefaultAsync();
+		}
+
+		public Task<int> SaveNoteAsync(Note note)
+		{
+			if (note.ID != 0)
+			{
+				return _database.UpdateAsync(note);
+			}
+			else
+			{
+				return _database.InsertAsync(note);
+			}
+		}
+
+		public Task<int> DeleteNoteAsync(Note note)
+		{
+			return _database.DeleteAsync(note);
+		}
+
+		public Task<int> DeleteAllNoteAsync(Note note)
+		{
+				return _database.ExecuteAsync("DELETE FROM Notes");
+			//	return _database.Table<Note>().ToListAsync();
+			//	return _database.Table<Note>();
+			//	return _database.DropTableAsync<Note>().Wait();
+
+		}
+	}
+}
